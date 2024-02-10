fa1f94ca94cdf1f4fdf67a9d775519d7131db82d
SaiSeng18 <saisengmongli14@gmail.com>
2024-02-10 23:22:30 +0630

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from "next/image";
+import { BiSearch } from "react-icons/bi";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className="flex-center h-screen w-full flex-col p-5">
+            <header className="mb-5 flex h-24 w-full justify-between rounded-3xl bg-gray-custom px-5">
+                <div className="flex items-center">
+                    <div>futurehome</div>
+                    <nav className="bg-white ml-40 rounded-3xl px-5 py-4">
+                        <ul className="flex items-center gap-5 font-medium">
+                            <li>
+                                <a href="">Shop</a>
+                            </li>
+                            <li>
+                                <a href="">Collection</a>
+                            </li>
+                            <li>
+                                <a href="">About</a>
+                            </li>
+                            <li>
+                                <a href="">FAQ</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <div className="flex items-center gap-5">
+                    <div className="flex w-20 flex-col gap-2">
+                        <div className="h-[2px] w-full bg-black"></div>
+                        <div className="h-[2px] w-full bg-black"></div>
+                    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                    <HiOutlineShoppingBag size={35} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                    <div className="bg-white flex-center gap-2 overflow-hidden rounded-full p-2">
+                        <BiSearch
+                            size={30}
+                            className="text-white rounded-full bg-black p-2"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Searching..."
+                            className="w-32 bg-transparent focus:outline-none"
+                        />
+                    </div>
+                </div>
+            </header>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <div className="flex-center h-full w-full gap-5">
+                <div className="flex h-full w-1/2 flex-col justify-between">
+                    <p className="text-7xl font-semibold">
+                        Combine style <br /> and innovation
+                    </p>
+                    <p className="w-2/3 text-lg">
+                        We offer a wide range of innovative and cutting-edge
+                        decor elements that will allow you to create a unique
+                        atmosphere and express your individually.
+                    </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                    <div className="flex h-14 gap-10">
+                        <button className="flex-between h-full w-fit gap-10 rounded-3xl border-2 border-primary bg-primary px-5">
+                            <p className="text-xl">Go to shop</p>
+                            <FaArrowRightLong />
+                        </button>
+                        <button className="flex-between h-full w-fit gap-10 rounded-3xl border-2 border-black px-5">
+                            <p className="text-xl">View video</p>
+                            <FaArrowRightLong />
+                        </button>
+                    </div>
+
+                    <div className="flex flex-col gap-5">
+                        <div className="flex items-center gap-10">
+                            <div className="flex h-[150px] gap-2 rounded-full bg-gray-custom p-2">
+                                <div className="aspect-square h-full rounded-full bg-gray-300"></div>
+                                <div className="aspect-square h-full rounded-full bg-gray-300"></div>
+                                <div className="aspect-square h-full rounded-full bg-gray-300"></div>
+                            </div>
+
+                            <div className="aspect-square h-[150px] rounded-full bg-black"></div>
+                        </div>
+
+                        <div className="flex h-[180px] items-center gap-5">
+                            <div className="flex-center h-full flex-col rounded-3xl bg-gray-custom p-5">
+                                <p className="text-5xl font-semibold">+ 150</p>
+                                <p>
+                                    item names in the <br /> new collection
+                                </p>
+                            </div>
+                            <div className="flex-center h-full gap-10 rounded-3xl bg-gray-custom p-5">
+                                <p className="max-w-[250px]">
+                                    With each collection, we strive to stand out
+                                    from the crowd and bring a fresh wind of
+                                    change
+                                </p>
+                                <div className="h-full w-44 rounded-3xl bg-gray-300"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="grid h-full w-1/2 grid-cols-2 gap-5">
+                    <div className="col-span-2 w-full rounded-3xl bg-primary"></div>
+                    <div className="w-full rounded-3xl bg-primary"></div>
+                    <div className="w-full rounded-3xl bg-primary"></div>
+                </div>
+            </div>
+        </main>
+    );
 }
